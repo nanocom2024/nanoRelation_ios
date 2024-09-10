a199dfb891aa052048c550d1d8dd0b668c8b4aee
shibat2 <x22049xx@aitech.ac.jp>
2024-09-10 15:47:44 +0900

    配列化しました。ボタンクリックでメッセージ、フレンド追加

--- nanocon02/FriendList.swift
+++ nanocon02/FriendList.swift
@@ -8,70 +8,70 @@
 import SwiftUI
 
 struct FriendList: View {
+    // メッセージの構造体
+    struct Friend: Identifiable {
+        let id = UUID()
+        let text: String
+        let image: Image
+    }
+    
+    // @Stateでメッセージのリストを管理
+    @State private var friends: [Friend] = []
+    
+    
+    
     var body: some View {
         VStack{//全体の縦構造
             HStack{//検索バーの部分
                 Spacer()
-                Image(systemName: "plus.circle")
-                    .font(Font.system(size: 35, weight: .regular))
+                Button(action: {
+                    friends.append(Friend(text: "なまえ", image: Image("charleyrivers")))
+                    
+                }) {
+                    Image(systemName: "plus.circle")
+                        .font(Font.system(size: 35, weight: .regular))
+                        .foregroundColor(.black)
+                }
+                
             }
             
-            NavigationLink(destination: Talk01()) {//下部アイコン01
-                HStack{//各個人
-                    Image("charleyrivers")
-                        .resizable()
-                        .frame(width: 70, height: 70)
-                        .clipShape(Circle())
+            ScrollView {//スクロールする領域を指定
+// ーーーーーーーーーーーーーーーーーmessages配列の要素をどのように並べるか、デザインーーーーーーーーーーーーーーーーーーー
+                ForEach(friends) { friend in//ForEach　配列の要素を構成するとき使い回しするもの
                     
-                    VStack(alignment: .leading){
-                        Text("なまえ")
-                            .font(.headline)
-                        Text("2024年1月20日 18時23分")
-                            .font(.subheadline)
+                    NavigationLink(destination: Talk01()) {
+                        HStack{//各個人
+                            friend.image
+                                .resizable()
+                                .frame(width: 70, height: 70)
+                                .clipShape(Circle())
+                            
+                            VStack(alignment: .leading){
+                                Text(friend.text) // 配列内のメッセージを表示
+                                Text("2024年1月20日 18時23分")
+                                    .font(.subheadline)
+                            }
+                            
+                            Spacer()
+                            
+                            Image(systemName: "figure.walk.circle")
+                                .padding(-3.0)
+                                .font(Font.system(size: 50, weight: .regular))
+                            
+                            Image(systemName: "speaker.wave.2.circle")
+                                .font(Font.system(size: 50, weight: .regular))
+                        }
+                        .foregroundColor(.black)
                     }
-                    
-                    Spacer()
-                    
-                    Image(systemName: "figure.walk.circle")
-                        .padding(-3.0)
-                        .font(Font.system(size: 50, weight: .regular))
-                    
-                    Image(systemName: "speaker.wave.2.circle")
-                        .font(Font.system(size: 50, weight: .regular))
-                }
-                .foregroundColor(.black)
-            }
+                }// messages配列の中身を表示
+// ーーーーーーーーーーーーーーーーーmessages配列の要素をどのように並べるか、デザインーーーーーーーーーーーーーーーーーーー
+            } // ScrollView
             
-            NavigationLink(destination: Talk01()) {//下部アイコン01
-                HStack{//各個人
-                    Image("charleyrivers")
-                        .resizable()
-                        .frame(width: 70, height: 70)
-                        .clipShape(Circle())
-                    
-                    VStack(alignment: .leading){
-                        Text("なまえ")
-                            .font(.headline)
-                        Text("2024年1月20日 18時23分")
-                            .font(.subheadline)
-                    }
-                    
-                    Spacer()
-                    
-                    Image(systemName: "figure.walk.circle")
-                        .padding(-3.0)
-                        .font(Font.system(size: 50, weight: .regular))
-                    
-                    Image(systemName: "speaker.wave.2.circle")
-                        .font(Font.system(size: 50, weight: .regular))
-                }
-                .foregroundColor(.black)
-            }
-            Spacer()
-        }
-        .padding(EdgeInsets(top:-15,leading: 20,bottom: -20,trailing:20))
-    }
+        }//var body: some View
+        .padding(EdgeInsets(top:0,leading: 20,bottom: 0,trailing:20))
+    }//struct Talk01: View
 }
+
 
 #Preview {
     FriendList()
--- nanocon02/Talk01.swift
+++ nanocon02/Talk01.swift
@@ -8,136 +8,124 @@
 import SwiftUI
 
 struct Talk01: View {
+    // メッセージの構造体
+    struct Message: Identifiable {
+        let id = UUID()
+        let text: String
+        let color: Color
+        let date: Date  // メッセージの日時
+    }
+    
+    // @Stateでメッセージのリストを管理
+    @State private var messages: [Message] = []
+    
     var body: some View {
-        VStack{//全体の縦構造
-            HStack{//プロフィール
-//                Image(systemName: "arrow.uturn.left")
-//                    .font(Font.system(size: 40, weight: .medium))
-//                    .padding(10)
-                Image("charleyrivers")
-                    .resizable()
-                    .frame(width: 60, height: 60)
-                    .clipShape(Circle())
-                    .padding(.leading,20)
-                Text("名前")
-                    .font(.title)
-                    .fontWeight(.semibold)
-                Spacer()
-            }
+        NavigationView {//これがないとtoolbarが使えない
+            VStack{//全体の縦構造
+                HStack{//プロフィール
+                    //                Image(systemName: "arrow.uturn.left")
+                    //                    .font(Font.system(size: 40, weight: .medium))
+                    //                    .padding(10)
+                    Image("charleyrivers")
+                        .resizable()
+                        .frame(width: 60, height: 60)
+                        .clipShape(Circle())
+                        .padding(.leading,20)
+                    Text("名前")
+                        .font(.title)
+                        .fontWeight(.semibold)
+                    Spacer()
+                }
+                
+                ScrollView {//スクロールする領域を指定
+// ーーーーーーーーーーーーーーーーーmessages配列の要素をどのように並べるか、デザインーーーーーーーーーーーーーーーーーーー
+                    ForEach(messages) { message in//ForEach　配列の要素を構成するとき使い回しするもの
+                        VStack { // メッセージ全体ーーーーーーーーーーーーー
+                            ZStack {
+                                RoundedRectangle(cornerRadius: 30)
+                                    .fill(message.color)
+                                    .frame(width: 370, height: 50)
+                                
+                                Text(message.text) // 配列内のメッセージを表示
+                                    .font(.title3)
+                                    .fontWeight(.semibold)
+                            }
+                            
+                            HStack { // 日時
+                                Spacer()
+                                Text(dateFormatter.string(from: message.date))
+                                    .padding(.top, -10)
+                                    .padding(.trailing, 25)
+                            } // 日時
+                            
+                            HStack{//未読メッセージアイコン
+                                Image(systemName: "exclamationmark.circle.fill")
+                                    .font(Font.system(size: 30, weight: .medium))
+                                    .foregroundColor(.red)
+                                    .padding(.top,-100)
+                                    .padding(.leading,5)
+                                Spacer()
+                            }//未読メッセージアイコン
+                        } // メッセージ全体ーーーーーーーーーーーーー
+                    }// messages配列の中身を表示
+// ーーーーーーーーーーーーーーーーーmessages配列の要素をどのように並べるか、デザインーーーーーーーーーーーーーーーーーーー
+                } // ScrollView
+                
+                .toolbar {
+                    ToolbarItemGroup(placement: .bottomBar) {
+                        Button(action: {
+                            // メッセージを追加（黄色）
+                            messages.append(Message(text: "愛知県中区○○○○　周辺ですれ違った", color: .yellow, date: Date()))
+                        }) {
+                            Text("すれ違い")
+                        }
+                        .padding()
+                        .accentColor(Color.black)
+                        .background(Color.yellow)
+                        .cornerRadius(26)
+                        
+                        Spacer()
+                        
+                        Button(action: {
+                            // メッセージを追加（赤）
+                            messages.append(Message(text: "なまえ　の子供が迷子になりました!", color: .red, date: Date()))
+                        }) {
+                            Text("迷子")
+                        }
+                        .padding()
+                        .accentColor(Color.black)
+                        .background(Color.red)
+                        .cornerRadius(26)
+                        
+                        Spacer()
+                        
+                        Button(action: {
+                            // メッセージを追加（緑）
+                            messages.append(Message(text: "愛知県中区○○○○　周辺で\nタロウ　とすれ違った", color: .green, date: Date()))
+                        }) {
+                            Text("迷子とすれ違い")
+                        }
+                        .padding()
+                        .accentColor(Color.black)
+                        .background(Color.green)
+                        .cornerRadius(26)
+                        
+                        Spacer()
+                    } // ToolbarItemGroup
+                } // .toolbar
+            } // NavigationView
             
-            VStack{//メッセージ全体ーーーーーーーーーーーーー
-                ZStack {
-                    RoundedRectangle(cornerRadius: 30)
-                        .fill(Color.yellow)
-                        .frame(width: 370, height: 50)
-                    
-                    Text("愛知県中区○○○○　周辺ですれ違った") // メッセージ
-                        .font(.title3)
-                        .fontWeight(.semibold)
-                }
-                
-                HStack {//日時
-                    Spacer()
-                    Text("2024年1月20日　18時23分")
-                        .padding(.top,-10)
-                        .padding(.trailing,25)
-                }//日時
-            }//メッセージ全体ーーーーーーーーーーーーー
             
-            VStack{//メッセージ全体ーーーーーーーーーーーーー
-                ZStack {
-                    RoundedRectangle(cornerRadius: 30)
-                        .fill(Color.yellow)
-                        .frame(width: 370, height: 50)
-                    
-                    Text("愛知県中区○○○○　周辺ですれ違った") // メッセージ
-                        .font(.title3)
-                        .fontWeight(.semibold)
-                }
-                
-                HStack {//日時
-                    Spacer()
-                    Text("2024年1月20日　18時23分")
-                        .padding(.top,-10)
-                        .padding(.trailing,25)
-                }//日時
-                
-                HStack{//未読メッセージアイコン
-                    Image(systemName: "exclamationmark.circle.fill")
-                        .font(Font.system(size: 30, weight: .medium))
-                        .foregroundColor(.red)
-                        .padding(.top,-100)
-                        .padding(.leading,5)
-                    Spacer()
-                }//未読メッセージアイコン
-            }//メッセージ全体ーーーーーーーーーーーーー
-            
-            VStack{//メッセージ全体ーーーーーーーーーーーーー
-                ZStack {
-                    RoundedRectangle(cornerRadius: 30)
-                        .fill(Color.yellow)
-                        .frame(width: 370, height: 50)
-                    
-                    Text("愛知県中区○○○○　周辺ですれ違った") // メッセージ
-                        .font(.title3)
-                        .fontWeight(.semibold)
-                }
-                HStack {//日時
-                    Spacer()
-                    Text("2024年1月20日　18時23分")
-                        .padding(.top,-10)
-                        .padding(.trailing,25)
-                }//日時
-                
-                HStack{//未読メッセージアイコン
-                    Image(systemName: "exclamationmark.circle.fill")
-                        .font(Font.system(size: 30, weight: .medium))
-                        .foregroundColor(.red)
-                        .padding(.top,-100)
-                        .padding(.leading,5)
-                    Spacer()
-                }//未読メッセージアイコン
-            }//メッセージ全体ーーーーーーーーーーーーー
-            
-            VStack{//　迷子　メッセージ全体ーーーーーーーーーーーーー
-                ZStack {
-                    RoundedRectangle(cornerRadius: 30)
-                        .fill(Color.red)
-                        .frame(width: 370, height: 50)
-                    
-                    Text("なまえ　の子供が迷子になりました!") // メッセージ
-                        .font(.title3)
-                        .fontWeight(.semibold)
-                }
-                
-                HStack {//日時
-                    Spacer()
-                    Text("2024年1月20日　18時23分")
-                        .padding(.top,-10)
-                        .padding(.trailing,25)
-                }//日時
-            }//メッセージ全体ーーーーーーーーーーーーー
-            VStack{//　迷子とすれ違い　メッセージ全体ーーーーーーーーーーーーー
-                ZStack {
-                    RoundedRectangle(cornerRadius: 30)
-                        .fill(Color.green)
-                        .frame(width: 370, height: 50)
-                    
-                    Text("愛知県中区○○○○　周辺で\nタロウ　とすれ違った") // メッセージ
-                        .font(.title3)
-                        .fontWeight(.semibold)
-                }
-                
-                HStack {//日時
-                    Spacer()
-                    Text("2024年1月20日　18時23分")
-                        .padding(.top,-10)
-                        .padding(.trailing,25)
-                }//日時
-            }//メッセージ全体ーーーーーーーーーーーーー
-            Spacer()
-        }
+        }//var body: some View
         .padding(EdgeInsets(top:0,leading: -5,bottom: -20,trailing:-5))
+    }//struct Talk01: View
+    
+    // 日時を日本形式で表示するためのDateFormatter
+    private var dateFormatter: DateFormatter {
+        let formatter = DateFormatter()
+        formatter.locale = Locale(identifier: "ja_JP")
+        formatter.dateFormat = "yyyy年MM月dd日 HH時mm分"
+        return formatter
     }
 }
 

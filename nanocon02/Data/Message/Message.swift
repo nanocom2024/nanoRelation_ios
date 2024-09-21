8e4ea826e5459822d48ff510b0332dcda55055c9
k22036 <131113333+k22036@users.noreply.github.com>
2024-09-21 12:31:04 +0900

    change: idを指定できるように

--- nanocon02/Data/Message/Message.swift
+++ nanocon02/Data/Message/Message.swift
@@ -10,8 +10,15 @@ import SwiftUI
 
 // メッセージの構造体
 struct Message: Identifiable {
-    let id = UUID()
+    let id: UUID
     let text: String
     let color: Color
     let date: Date  // メッセージの日時
+    
+    init(id: UUID = UUID(), text: String, color: Color, date: Date) {
+        self.id = id
+        self.text = text
+        self.color = color
+        self.date = date
+    }
 }
